0bac52a9fda3ae7980a41d366c7b57b692a5fd3d
David Hamm <david.hamm@dhweb.xyz>
2019-10-14 06:26:47 -0500

    Added function to clear table before posting results

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,9 @@
+
+
 $('#denomSubmit').click(function(event) {
     event.preventDefault();
     $.post('processes/denominate.php', $('#denomForm').serialize(), function(data) {
+        clearTableValues();
         $('#silverDollar').html(data.ones);
         $('#halfDollar').html(data.halfDollars);
         $('#quarter').html(data.quarters);
@@ -8,4 +11,13 @@ $('#denomSubmit').click(function(event) {
         $('#nickel').html(data.nickels);
         $('#penny').html(data.pennies);
     });
-})
+});
+
+function clearTableValues() {
+    $('#silverDollar').html(0);
+    $('#halfDollar').html(0);
+    $('#quarter').html(0);
+    $('#dime').html(0);
+    $('#nickel').html(0);
+    $('#penny').html(0);
+}
