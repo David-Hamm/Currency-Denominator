04d2fc3ab0d7d84ceb6d2f7564bf43b490bb6509
David Hamm <david.hamm@dhweb.xyz>
2019-10-14 08:40:51 -0500

    Update denominate.php

--- processes/denominate.php
+++ processes/denominate.php
@@ -9,7 +9,6 @@ if (!isset($_POST['dollarAmount']) || !is_numeric($_POST['dollarAmount'])) {
     ]);
 } else {
     $currency = new Currency('US', $_POST['dollarAmount'], 5);
-    // Array indices to find denominations are found in the currency class
     header('Content-type: application/json');
     echo json_encode($currency->getCurrencyDistribution());
 }
